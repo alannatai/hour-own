bfe2a8be8a330ca787d3cc8c263f9ca27415e98a
Alanna Tai <alanna.tai@hotmail.com>
2020-02-16 17:50:22 -0500

    Login form complete, able to login successfully

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -2,50 +2,80 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import userService from '../../utils/userService';
 
+import './SignupForm.css';
+
 class SignupForm extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: '',
-    passwordConfirm: ''
-  };
+	state = {
+		name: '',
+		email: '',
+		password: '',
+		passwordConfirm: ''
+	};
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
+	handleChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
 
-  handleSubmit = async(e) => {
-    e.preventDefault();
-    try {
-      await userService.signup(this.state);
-      this.props.handleSignupOrLogin();
-      this.props.history.push('/')
-    } catch(err) {
-      console.log(err)
-    }
-  }
+	handleSubmit = async e => {
+		e.preventDefault();
+		try {
+			await userService.signup(this.state);
+			this.props.handleSignupOrLogin();
+			this.props.history.push('/');
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  isFormInvalid() {
-    return !(this.state.name && this.state.email && this.state.password === this.state.passwordConfirm);
-  }
+	isFormInvalid() {
+		return !(
+			this.state.name &&
+			this.state.email &&
+			this.state.password === this.state.passwordConfirm
+		);
+	}
 
-  render() {
-    return (
-      <>
-        <header>Sign up</header>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Name" value={this.state.name} name="name" onChange={this.handleChange}/>
-          <input type="email" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange}/>
-          <input type="password" placeholder="Password" value={this.state.password} name="password" onChange={this.handleChange}/>
-          <input type="password" placeholder="Confirm Password" value={this.state.passwordConfirm} name="passwordConfirm" onChange={this.handleChange}/>
-          <button disabled={this.isFormInvalid()}>Sign Up</button>
-          <Link to='/'>Cancel</Link>
-        </form>
-      </>
-    )
-  }
-};
+	render() {
+		return (
+			<>
+				<header>Sign up</header>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						placeholder="Name"
+						value={this.state.name}
+						name="name"
+						onChange={this.handleChange}
+					/>
+					<input
+						type="email"
+						placeholder="Email"
+						value={this.state.email}
+						name="email"
+						onChange={this.handleChange}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						value={this.state.password}
+						name="password"
+						onChange={this.handleChange}
+					/>
+					<input
+						type="password"
+						placeholder="Confirm Password"
+						value={this.state.passwordConfirm}
+						name="passwordConfirm"
+						onChange={this.handleChange}
+					/>
+					<button disabled={this.isFormInvalid()}>Sign Up</button>
+					<Link to="/">Cancel</Link>
+				</form>
+			</>
+		);
+	}
+}
 
 export default SignupForm;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import userService from '../../utils/userService';
+
 import './LoginPage.css';
 
-const LoginPage = () => {
-  return (
-    <div className='LoginPage'>
-      LOG IN PAGE
-    </div>
-  ); 
+class LoginPage extends Component {
+	state = {
+		email: '',
+		password: ''
+	};
+
+	handleChange = e => {
+		this.setState({ 
+      [e.target.name]: e.target.value 
+    });
+	};
+
+	handleSubmit = async e => {
+		e.preventDefault();
+		try {
+			await userService.login(this.state);
+			this.props.handleSignupOrLogin();
+			this.props.history.push('/');
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	render() {
+		return (
+			<div className="LoginPage">
+				<header>Log In</header>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="email"
+						placeholder="Email"
+						value={this.state.email}
+						name="email"
+						onChange={this.handleChange}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						value={this.state.password}
+						name="password"
+						onChange={this.handleChange}
+					/>
+					<button>Log In</button>
+					<Link to="/">Cancel</Link>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default LoginPage;
